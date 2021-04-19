112e4a06a6793a19c1e1bb428162751c807e62eb
krush11 <krushnalpatel11@gmail.com>
2021-04-19 18:25:18 +0530

    enhance views

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,5 +1,5 @@
 function load() {
     var heading = document.getElementById("header");
     heading.style.boxShadow = "0px 5px 14px 0px rgba(0,0,0,0.4)";
-    heading.style.backgroundColor = "rgb(241, 241, 241)"
+    heading.style.backgroundColor = "white";
 }
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -13,19 +13,3 @@ function home() {
         nav_bar.style.backgroundColor = "unset";
     }
 }
-
-function other_views() {
-    var y = document.getElementById("body").scrollTop;
-    console.log(y);
-    var nav_bar = document.getElementById("header");
-    var logo = document.getElementById("logo");
-    
-    if (y > 0) {
-        nav_bar.style.animation = "scroll_down 0s 1 ";
-        nav_bar.style.backgroundColor = "rgb(241, 241, 241)";
-    }
-    else if (y == 0) {
-        nav_bar.style.animation = "scroll_up 0s 1 ";
-        nav_bar.style.backgroundColor = "rgb(241, 241, 241)";
-    }
-}
