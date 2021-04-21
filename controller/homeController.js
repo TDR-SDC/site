d933315ac4575875c53bc36ba569830b0de41b85
krush11 <krushnalpatel11@gmail.com>
2021-04-21 19:35:48 +0530

    update header

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,23 +1,49 @@
+const Users = require('../models/users');
+
 module.exports.home = function (req, res) {
-    res.render('home');
+    Users.findById(req.user._id, function (err, user) {
+        return res.render('home', {
+            profile_user: user
+        });
+    });
 };
 
 module.exports.team = function (req, res) {
-    res.render('team');
+    Users.findById(req.user._id, function (err, user) {
+        return res.render('team', {
+            profile_user: user
+        });
+    });
 };
 
 module.exports.sponsors = function (req, res) {
-    res.render('sponsors');
+    Users.findById(req.user._id, function (err, user) {
+        return res.render('sponsors', {
+            profile_user: user
+        });
+    });
 };
 
 module.exports.gallery = function (req, res) {
-    res.render('gallery');
+    Users.findById(req.user._id, function (err, user) {
+        return res.render('gallery', {
+            profile_user: user
+        });
+    });
 };
 
 module.exports.contact = function (req, res) {
-    res.render('contact');
+    Users.findById(req.user._id, function (err, user) {
+        return res.render('contact', {
+            profile_user: user
+        });
+    });
 };
 
 module.exports.not_found = function (req, res) {
-    res.render('error_404');
+    Users.findById(req.user._id, function (err, user) {
+        return res.render('error_404', {
+            profile_user: user
+        });
+    });
 }
--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,11 @@
+const Users = require('../models/users');
+
 module.exports.profile = function (req, res) {
-    if(req.isAuthenticated())
-        res.send('Hello');
+    if (req.isAuthenticated())
+    Users.findById(req.user._id, function (err, user) {
+        return res.render('profile', {
+            profile_user: user
+        });
+    });
     else res.redirect('/login');
 };
