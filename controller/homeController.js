47657b908e5e62394f0cc967aecce2a0f363e4a4
krush11 <krushnalpatel11@gmail.com>
2021-04-22 00:57:36 +0530

    removed redundant code

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,67 +1,29 @@
 const Users = require('../models/users');
 
 module.exports.home = function (req, res) {
-    if (req.isAuthenticated()) {
-        Users.findById(req.user._id, function (err, user) {
-            return res.render('home', {
-                profile_user: user
-            });
-        });
-    }
-    else return res.render('home');
+    return res.render('home');
 };
 
 module.exports.team = function (req, res) {
-    if (req.isAuthenticated()) {
-        Users.findById(req.user._id, function (err, user) {
-            return res.render('team', {
-                profile_user: user
-            });
+    Users.find({}, function (err, user) {
+        return res.render('team', {
+            users: user
         });
-    }
-    else return res.render('team');
+    })
 };
 
 module.exports.sponsors = function (req, res) {
-    if (req.isAuthenticated()) {
-        Users.findById(req.user._id, function (err, user) {
-            return res.render('sponsors', {
-                profile_user: user
-            });
-        });
-    }
-    else return res.render('sponsors');
+    return res.render('sponsors');
 };
 
 module.exports.gallery = function (req, res) {
-    if (req.isAuthenticated()) {
-        Users.findById(req.user._id, function (err, user) {
-            return res.render('gallery', {
-                profile_user: user
-            });
-        });
-    }
-    else return res.render('gallery');
+    return res.render('gallery');
 };
 
 module.exports.contact = function (req, res) {
-    if (req.isAuthenticated()) {
-        Users.findById(req.user._id, function (err, user) {
-            return res.render('contact', {
-                profile_user: user
-            });
-        });
-    }
-    else return res.render('contact');
+return res.render('contact');
 };
 
 module.exports.not_found = function (req, res) {
-    if (req.isAuthenticated()) {
-        Users.findById(req.user._id, function (err, user) {
-            return res.render('error_404', {
-                profile_user: user
-            });
-        });
-    }
-    else return res.render('error_404');
+return res.render('error_404');
 }
