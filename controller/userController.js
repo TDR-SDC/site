3eaafd86021facb73d08e2aff46a26cd0f28e074
krush11 <krushnalpatel11@gmail.com>
2021-04-23 19:59:09 +0530

    added conditional permission grant functionality

--- controller/userController.js
+++ controller/userController.js
@@ -27,13 +27,30 @@ module.exports.upload_avatar = async function (req, res) {
 };
 
 module.exports.create = async function (req, res) {
+    var permission = 10;
+    if (req.body.year == 1)
+        permission = 9;
+    if (req.body.year == 2)
+        permission = 8;
+    if (req.body.year == 3)
+        permission = 7;
+    if (req.body.year == 4)
+        permission = 10;
+    if (req.body.position.toLowerCase().includes('lead'))
+        permission = 3;
+    if (req.body.position.toLowerCase().includes('manager'))
+        permission = 2;
+    if (req.body.position.toLowerCase().includes('team lead'))
+        permission = 1;
+
     await Users.create({
         "user": req.body.user,
         "name": req.body.name,
         "password": req.body.password,
         "dept": req.body.dept,
         "year": req.body.year,
-        "position": req.body.position
+        "position": req.body.position,
+        "permission": permission
     });
     res.redirect('/user/profile');
 };
@@ -56,16 +73,20 @@ module.exports.update_credentials = async function (req, res) {
     res.redirect('back');
 }
 
+// This function is solely for testing and mannual DB changes.
+// Tread carefully over this function.
 module.exports.user_info = async function (req, res) {
-
-    await Users.find({}, function (err, user) {
-        if (user.year == 2) {
-            user.permission = 9;
-            Users.findByIdAndUpdate(user._id, { permission: 9 });
-    }
+    if (req.isAuthenticated() && (req.user.user == 'krush' || req.user.permission == 0)) {
+        await Users.find({}, (err, user) => {
             res.send(user);
-
-})
+        });
+    }
+    else
+        res.render('error', {
+            error: true,
+            error_code: 403,
+            error_message: "Forbidden!! You do not have access to this page."
+        });
 };
 
 module.exports.logout = function (req, res) {
