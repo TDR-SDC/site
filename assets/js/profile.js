39ba5497e8162c2887a29df8bde591c24404acee
Krushnal Patel <krushnalpatel11@gmail.com>
2021-05-18 22:28:04 +0530

    redesigned page

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,8 +1,8 @@
-var current_profile_element = document.getElementById('display_update_profile');
+var current_profile_element = document.getElementById('profile');
 
 function show_profile_section(profile_section) {
     var element = document.getElementById(profile_section);
     current_profile_element.style.display = "none";
-    element.style.display = "revert";
+    element.style.display = "block";
     current_profile_element = element;
 }
