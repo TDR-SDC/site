e27209fae7ce7e9bc4b046cb241fc89e385092bc
Krushnal Patel <krushnalpatel11@gmail.com>
2021-06-13 23:45:21 +0530

    added about FS section

--- controller/homeController.js
+++ controller/homeController.js
@@ -5,58 +5,62 @@ const Photos = require('../models/photo');
 const Newsletter = require('../models/newsletter');
 
 module.exports.home = function (req, res) {
-    Newsletter.find().sort({ date: -1 }).then((newsletters) => {
-        let monthNames = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ];
-        return res.status(200).render('home', {
-            newsletters: newsletters,
-            monthNames: monthNames
-        });
-    })
+	Newsletter.find().sort({ date: -1 }).then((newsletters) => {
+		let monthNames = ["January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"
+		];
+		return res.status(200).render('home', {
+			newsletters: newsletters,
+			monthNames: monthNames
+		});
+	})
+};
+
+module.exports.about = function (req, res) {
+	return res.status(200).render('about');
 };
 
 module.exports.team = function (req, res) {
-    Users.find().sort({ permission: 1, name: 1 }).then((user) => {
-        Alum.find().sort({ name: 1 }).then((alumni) => {
-            res.status(200).render('team', {
-                users: user,
-                alumni: alumni
-            });
-        });
-    });
+	Users.find().sort({ permission: 1, name: 1 }).then((user) => {
+		Alum.find().sort({ name: 1 }).then((alumni) => {
+			res.status(200).render('team', {
+				users: user,
+				alumni: alumni
+			});
+		});
+	});
 };
 
 module.exports.gallery = function (req, res) {
-    Photos.find({}, function (err, photos) {
-        res.status(200).render('gallery', {
-            photos: photos
-        })
-    });
+	Photos.find({}, function (err, photos) {
+		res.status(200).render('gallery', {
+			photos: photos
+		})
+	});
 };
 
 module.exports.contact = function (req, res) {
-    return res.status(200).render('contact');
+	return res.status(200).render('contact');
 };
 
 module.exports.contact_form = function (req, res) {
-    const contact = new Contact();
-    contact.name = req.body.name;
-    contact.email = req.body.name;
-    contact.subject = req.body.subject;
-    contact.message = req.body.message;
-    contact.save().then(promiseResult => {
-        if (promiseResult !== contact)
-            res.status(502).render('error', {
-                error: true,
-                error_code: 502,
-                error_message: "Bad Gateway!! Unable to submit contact request to server."
-            })
-        else
-            res.status(302).redirect('back');
-    });
+	const contact = new Contact();
+	contact.name = req.body.name;
+	contact.email = req.body.name;
+	contact.subject = req.body.subject;
+	contact.message = req.body.message;
+	contact.save().then(promiseResult => {
+		if (promiseResult !== contact)
+			res.status(502).render('error', {
+				error: true,
+				error_code: 502,
+				error_message: "Bad Gateway!! Unable to submit contact request to server."
+			})
+		else
+			res.status(302).redirect('back');
+	});
 };
 
 module.exports.not_found = function (req, res) {
-    return res.status(404).render('error');
+	return res.status(404).render('error');
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controller/homeController');
 
 router.get('/', homeController.home);
+router.get('/about_fs', homeController.about);
 router.get('/team', homeController.team);
 router.get('/gallery', homeController.gallery);
 router.get('/contact', homeController.contact);
